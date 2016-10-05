9fc08bfa1d1a19b76c5c2164239117d21ce5ceed
Robert Ventura <rventura@lludrigasoft.cat>
2016-10-05 23:33:50 +0200

    Afegeixo contingut al fitxer de centos70-install-wiki i modifico la creacio de la BD

--- centos70-install-percona-server.sh
+++ centos70-install-percona-server.sh
@@ -30,7 +30,7 @@ SECURE_MYSQL=$(expect -c "
 set timeout 10
 spawn mysql_secure_installation
 expect \"Enter current password for root (enter for none):\"
-send \"$MYSQL\r\"
+send \"$MYSQL_ROOT_PASSWORD\r\"
 expect \"Change the root password?\"
 send \"n\r\"
 expect \"Remove anonymous users?\"
--- centos70-install-wiki.sh
+++ centos70-install-wiki.sh
@@ -11,17 +11,46 @@ url_scripts="https://raw.githubusercontent.com/robertventura/scripts/master/"
 
 
 ## Realitzem un UPDATE dels paquets
-curl -fsSL ${url_scripts}update-centos70.sh | sh
+curl -fsSL ${url_scripts}centos70-update.sh | sh
 
 ## Instal·lació d'Apache
-curl -fsSL ${url_scripts}install-apache-centos70.sh | sh
+curl -fsSL ${url_scripts}cento70-install-apache.sh | sh
 
 ## Instal·lació d'Percona MySQL
-curl -fsSL ${url_scripts}install-percona-server-centos70.sh | sh
+curl -fsSL ${url_scripts}centos70-install-percona-server.sh | sh
 
 ## Instal·lació PHP
-curl -fsSL ${url_scripts}install-php-centos70.sh | sh
+curl -fsSL ${url_scripts}centos70-install-php.sh | sh
 
+## Extensions necessàries de PHP
+yum install -y php-xml php-intl php-gd texlive
+
+#yum install epel-release php-xcache
+
+sudo systemctl restart httpd.service
+
+curl -O http://releases.wikimedia.org/mediawiki/1.27/mediawiki-1.27.1.tar.gz
+
+tar xvzf mediawiki-*.tar.gz
+
+mkdir -p /var/www/html/mediawiki
+
+mv mediawiki-1.27.1/* /var/www/html/mediawiki
+
+chown -R apache:apache /var/www/html/mediawiki
+
+export DB_NAME=wiki
+export DB_USER=wiki
+export DB_PWD=P0t@t0es
+
+mysql -u root --password="$DB_PWD" << EOF
+CREATE DATABASE $DB_NAME;
+CREATE USER '$DB_USER'@'localhost' IDENTIFIED BY '$DB_PWD';
+GRANT INDEX, CREATE, SELECT, INSERT, UPDATE, DELETE, ALTER, LOCK TABLES ON wiki.* TO '$DB_USER'@'localhost';
+FLUSH PRIVILEGES;
+EOF
+
+#Faltaria editar els fitxers de configuració
 
 	
 
